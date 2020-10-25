b74f39edfd822a2570db669fc6e289037ff7d0ca
Marina <mbakanova2016@yandex.ru>
2020-10-25 14:56:06 +0300

    added svipes

---
+++ Assets/Scripts/GyroCheck.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroCheck : MonoBehaviour
+{
+   // public GameObject player; // First Person Controller parent node
+    public GameObject head; // First Person Controller camera
+
+    private int initialOrientationX;
+    private int initialOrientationY;
+   // private int initialOrientationZ;
+
+   // public void ReturnToPreviosRotate(){
+      //  head.transform.eulerAngles = new Vector3(0, 0, 0);
+   // }
+
+    void Start() {
+        Input.gyro.enabled = true;
+        initialOrientationX = (int)Input.gyro.rotationRateUnbiased.x;
+        initialOrientationY = (int)Input.gyro.rotationRateUnbiased.y;
+       // initialOrientationZ = (int)(-Input.gyro.rotationRateUnbiased.z);
+    }
+
+    void FixedUpdate()
+    {
+        //player.transform.Rotate(0, initialOrientationY - Input.gyro.rotationRateUnbiased.y, 0);
+        //head.transform.Rotate(initialOrientationX - Input.gyro.rotationRateUnbiased.x, 0, initialOrientationZ + Input.gyro.rotationRateUnbiased.z);
+      //  player.transform.position = new Vector3(0,0 , 0);
+        head.transform.position = new Vector3 ((initialOrientationX - Input.gyro.rotationRateUnbiased.x)/3, 0, 0);
+    }
+
+}
---
+++ Assets/Scripts/SvaipCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+using UnityEngine.UI;
+
+
+    public class SvaipCamera : MonoBehaviour
+    {
+    public float touchRotAdjust = 1f;
+
+    void Update()
+    {
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Add this part after the other Rotation lines
+            transform.Rotate(0f, (touchDeltaPosition.x * touchRotAdjust)/3, 0f, Space.Self);
+        }
+
+    }
+}
