f0522e2b9191f272f0b8d2b68a3b550551e825ab
Marina <mbakanova2016@yandex.ru>
2021-02-13 21:53:04 +0300

    deleted oop tryings

--- Assets/Scripts/DestroyMarks.cs
+++ Assets/Scripts/DestroyMarks.cs
@@ -8,29 +8,7 @@ public class DestroyMarks : MonoBehaviour
     public int number_marks1;
     public int number_marks2;
     public int number_marks3;
-    public void set_number_marks(int number_marks)
-    {
-        this.number_marks = number_marks;
-    }
-    public void set_number_marks1(int number_marks1)
-    {
-        this.number_marks1 = number_marks1;
-    }
-    public void set_number_marks2(int number_marks2)
-    {
-        this.number_marks2 = number_marks2;
-    }
-    public void set_number_marks3(int number_marks3)
-    {
-        this.number_marks3 = number_marks3;
-    }
-    public DestroyMarks(int number_marks, int number_marks1, int number_marks2, int number_marks3)
-    {
-        this.number_marks = number_marks;
-        this.number_marks1 = number_marks1;
-        this.number_marks2 = number_marks2;
-        this.number_marks3 = number_marks3;
-    }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -48,26 +26,25 @@ public class DestroyMarks : MonoBehaviour
     }
     void OnMouseDown()
     {
-        DestroyMarks destroyMarks = new DestroyMarks(0, 0, 0, 0);
         if (gameObject.name.Contains("Sphere1"))
         {
-            destroyMarks.set_number_marks1(1);
+            number_marks1++;
             Destroy(gameObject);
             Debug.Log("first"+number_marks1);
         }
         if (gameObject.name.Contains("Sphere2"))
         {
-            destroyMarks.set_number_marks2(1);
+            number_marks2++;
             Destroy(gameObject);
             Debug.Log("second"+number_marks2);
         }
         if (gameObject.name.Contains("Sphere3"))
         {
-            destroyMarks.set_number_marks3(1);
+            number_marks3++;
             Destroy(gameObject);
             Debug.Log("third"+number_marks3);
         }
-        destroyMarks.set_number_marks(number_marks1 + number_marks2 + number_marks3);
+        number_marks = number_marks1 + number_marks2 + number_marks3;
         Debug.Log("final"+number_marks);
 
     } 
