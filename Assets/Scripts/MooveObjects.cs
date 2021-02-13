5459b9b9bd12bfd4a0bfc1e54ba2400504355a6d
Marina <mbakanova2016@yandex.ru>
2021-02-13 20:36:39 +0300

    script for mooving objects

---
+++ Assets/Scripts/MooveObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MooveObjects : MonoBehaviour
+{
+    public int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        i++;
+        if (i % 2 != 0) {
+            gameObject.transform.Translate(1, 0, 0);
+        }
+        else {
+            gameObject.transform.Translate(-1, 0, 0);
+        }
+        
+
+    }
+}
